6ff1e4daec719cdafef34654f24cdb2c8835e6cd
SaraPavone <sara.pavone.92@gmail.com>
2024-09-02 16:36:48 +0200

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    jobs: [],
+}
+
+
+const mainReducer = (state = {}, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default mainReducer;
